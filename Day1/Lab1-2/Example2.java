2eafbc89ee6a5934fcf310eaa77ce2cb2d5c72c2
Mai Yasser <maiymuhammad8@gmail.com>
2023-01-25 12:57:33 +0200

    day1

---
+++ Day1/Lab1-2/Example2.java
@@ -0,0 +1,11 @@
+public class Example2
+{
+    public static void main(String[] args)
+    {
+        if (args[0].equals("CoreJava"))
+        {
+            System.out.println("true value "+args[0]);
+        }
+       
+    }
+}
