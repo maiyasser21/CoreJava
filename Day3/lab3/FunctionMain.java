aa1c2d85ad6f63ffc3d44cae281ffeb2c75d0421
Mai Yasser <maiymuhammad8@gmail.com>
2023-02-11 02:51:22 +0200

    Day3,lab3

---
+++ Day3/lab3/FunctionMain.java
@@ -0,0 +1,8 @@
+public class FunctionMain {
+    public static void main(String[] args) {
+        float arr[]={1,0,-9};
+        FunctionTask fn=new FunctionTask();
+        fn.apply(arr);
+    }
+    
+}
